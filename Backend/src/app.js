4d23f33376e7e16fb535bc41690da743583d3572
Escape <abdurrahmanshaikh121212@gmail.com>
2026-03-06 12:41:11 +0530

    fix cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,10 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-     origin: [
-        "http://localhost:5173",
-        "https://ai-mock-interview-pla-git-f02aeb-abdurrahmans-projects-67825f61.vercel.app"
-    ],
+     origin: true,
     credentials: true
 }))
 
